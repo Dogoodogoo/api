f6784ca92814be3b04e6cfd602b4f5ec5e10739c
jhin <joojh501@gmail.com>
2026-03-18 20:43:34 +0900

    chore(swagger): API 명세서 메타데이터 설정 및 개발 서버 정보 추가

--- src/main/java/com/github/dogoodogoo/api/config/SwaggerConfig.java
+++ src/main/java/com/github/dogoodogoo/api/config/SwaggerConfig.java
@@ -3,21 +3,32 @@ package com.github.dogoodogoo.api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .title("어디가개(Dogoodogoo) API 명세서")
+                .description("반려견 산책 경로 추천 및 주변 공간 시설(PostGIS) 조회 API")
+                .version("v1.0.0")
+                .contact(new Contact()
+                        .name("JhinsLog")
+                        .url("https://github.com/Dogoodogoo/api/tree/develop"));
+
+        Server devServer = new Server()
+                .url("http://jhin.iptime.org:8080")
+                .description("개발 서버");
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("어디가개(DogooDogoo) API 명세서")
-                        .description("반려견 산책 경로 추천 및 주변 공간 시설(PostGIS) 조회 API")
-                        .version("v1.0.0")
-                        .contact(new Contact()
-                                .name("주진홍")
-                                .url("[https://github.com/dogoodogoo](https://github.com/dogoodogoo)")));
+                .info(info)
+                .servers(List.of(devServer));
     }
 }
